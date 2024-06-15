fe232326804e729bee5493ca52252474ed6a5f14
sarkheel21429 <your_email@example.com>
2024-06-16 03:19:59 +0500

    mvmnbmc

--- Server.js
+++ Server.js
@@ -9,18 +9,26 @@ const CreateProduct = require("./routes/ProductRoutes");
 const AddToCart = require("./routes/AddToCartRoutes");
 const CheckOut = require("./routes/OrderRoutes");
 const Message = require("./routes/MessagesRoutes");
-// const path = require("path");
+const path = require("path");
 
-//configure env
+// Configure environment variables
 dotenv.config();
 
-//databse config
+// Database configuration
 connectDB();
 
-//rest object
+// Create an Express application
 const app = express();
+
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Static files for frontend (Uncomment if you have a frontend build directory)
 // app.use(express.static(path.join(__dirname, "./client/build")));
 
+// Uncomment if you want to serve frontend files
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"), function (err) {
 //     if (err) {
@@ -30,23 +38,18 @@ const app = express();
 //   });
 // });
 
-
-//middelwares
-app.use(cors());
-app.use(express.json());
-app.use(morgan("dev"));
-
-//routes 
+// Define routes
 app.use("/api/auth", authRoutes);
 app.use("/api/auth/category", CategoryRoutes);
 app.use("/api/auth/product", CreateProduct);
 app.use("/api/auth/cart", AddToCart);
 app.use("/api/auth/checkout", CheckOut);
 app.use("/api/auth/message", Message);
-//PORT1
-const PORT = process.env.PORT
 
-//run listen
+// Get the port from environment variables or default to 8080
+const PORT = process.env.PORT || 8080;
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server Running on http://localhost:${PORT}`);
 });
